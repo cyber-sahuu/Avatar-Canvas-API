f6ffbf1513858db3cd485b420cf37348b5769b02
SA HU <fbshahadatislam@gmail.com>
2026-01-22 22:29:07 +0600

    Update and rename love3.js to couple.js

--- Canvas/couple.js
+++ Canvas/couple.js
@@ -3,7 +3,7 @@ const fs = require("fs-extra");
 const path = require("path");
 
 module.exports.config = {
-  name: "love3",
+  name: "couple",
   version: "1.0.0",
   hasPermssion: 0,
   credits: "SHAHADAT SAHU",
